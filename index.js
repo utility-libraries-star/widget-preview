cf4084924c0bb8a8622c235fafb1b57c2fd012a0
utility-libraries-star <114432586+utility-libraries-star@users.noreply.github.com>
2024-07-09 15:37:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const installPlatform = () => {
+  const script = document.createElement('script');
+  script.setAttribute(
+    'src',
+    'https://static.elfsight.com/platform/platform.js',
+  );
+  script.setAttribute('data-use-service-core', 'true');
+  script.setAttribute('defer', 'true');
+  document.head.appendChild(script);
+};
+
+const installWidget = (widgetId) => {
+  const widget = document.createElement('div');
+  widget.classList.add(`elfsight-app-${widgetId}`);
+  document.body.prepend(widget);
+};
+
+const searchParams = new URLSearchParams(window.location.search);
+const widgetId = searchParams.get('widgetId');
+if (widgetId) {
+  installPlatform();
+  installWidget(widgetId);
+}
